8e872eca0d96763fdff83a42d7a9bd200ea46678
Jon Banafato <jonathan.banafato@gmail.com>
2013-06-29 03:45:33 -0400

    add secret key, since we'll probably eventually need it

--- src/core/settings.py
+++ src/core/settings.py
@@ -4,7 +4,9 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 
 DEBUG = False
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://nycpython:nycpython@localhost:5432/subwars'
+SECRET_KEY = b'\x7f\xebt.Y\xea\xd5\x19"\x1ftu5\x9d\xaa.FD\xac5\xbc\x86\xe7c=\xc5_\xfe\xf5\x96\x1eH'
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://nycpython:nycpython@localhost:5432/nycpython'
 TEMPLATE_FOLDER = os.path.join(PROJECT_ROOT, 'templates')
 STATIC_FOLDER = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL_PATH = '/static'
