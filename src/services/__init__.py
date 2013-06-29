a6b069599f02da5b369900ab346e5f8a3a58bd64
Saul Diez-Guerra <saul@diezguerra.com>
2013-06-29 14:21:54 -0400

    services and services.meetup modules

---
+++ src/services/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+    services
+    ~~~~~~~~
+
+    Contains services to be used by the main NYCPython application
+
+"""
---
+++ src/services/meetup/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+    services.meetup
+    ~~~~~~~~~~~~~~~
+
+    Contains services interfacing with the meetup.com API
+
+"""
